f0259925b2c5176b3774cfc85606f9815dbeb727
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-05-17 23:08:05 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+let num = 0
+OLED12864_I2C.init(60)
+OLED12864_I2C.zoom(false)
+basic.forever(function () {
+    OLED12864_I2C.showString(
+    0,
+    0,
+    "Hello!",
+    1
+    )
+    OLED12864_I2C.rect(
+    0,
+    num,
+    20,
+    10,
+    1
+    )
+    OLED12864_I2C.showNumber(
+    5,
+    2,
+    num,
+    1
+    )
+    OLED12864_I2C.pixel(num, num, 1)
+    num += 1
+    basic.pause(500)
+})
